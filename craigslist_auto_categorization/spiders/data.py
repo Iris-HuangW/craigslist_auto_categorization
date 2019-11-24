e3e3bd7cb696e4f534051ddb2c14a72f7916a357
bbbbirishwy <huang814@purdue.edu>
2019-11-24 17:40:08 -0500

    Initial commit

---
+++ craigslist_auto_categorization/spiders/data.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy import Request
+# Final Project - scrapy
+
+class CraigslistSpider(scrapy.Spider):
+    name = 'data'
+    allowed_domains = ['craigslist.org']
+    start_urls = ['https://chicago.craigslist.org/']
+    # scrap ad category from the current layer
+    def parse(self, response):
+        cat = response.xpath('//*[@id="sss"]//*[@class="cats"]//li')
+        for c in cat:
+            category = c.xpath('a/span/text()').extract()
+            lower_rel_url = c.xpath('a/@href').extract_first()
+            lower_url = response.urljoin(lower_rel_url)
+            yield Request(lower_url, callback=self.parse_lower, 
+                meta={'Category': category})
+    # scrap ad title from the next layer 
+    def parse_lower(self, response): 
+        ads = response.xpath('//p[@class="result-info"]')
+        for ad in ads:  # extract each ad info
+            title = ad.xpath('a/text()').extract()
+            response.meta['Title'] = title
+            category = response.meta.get('Category')
+            lower_rel_url = ad.xpath('a/@href').extract_first()
+            lower_rul = response.urljoin(lower_rel_url)
+            yield Request(lower_rul, callback=self.parse_sec_lower, 
+                meta={'Title':title,'Category': category})
+        #  scrape from multiple pages
+        next_rel_url = response.xpath('//a[@class="button next"]/@href').get()
+        next_url = response.urljoin(next_rel_url)
+        yield Request(next_url, callback=self.parse_lower)
+    # scrap ad description from the second layer
+    def parse_sec_lower(self, response):
+        text = "".join(line for line in response.xpath('//*[@id="postingbody"]/text()').getall()) 
+        text = text.lstrip()
+        response.meta['Description'] = text 
+        yield response.meta
+
